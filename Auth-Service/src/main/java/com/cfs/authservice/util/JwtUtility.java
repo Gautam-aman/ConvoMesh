ab36fc75c364f4d784365067d4936a9c0f08e2f9
Aman Gautam <amangautam2128@gmail.com>
2026-03-28 21:18:08 +0530

    fixed gateway

--- Auth-Service/src/main/java/com/cfs/authservice/util/JwtUtility.java
+++ Auth-Service/src/main/java/com/cfs/authservice/util/JwtUtility.java
@@ -4,13 +4,18 @@ import com.cfs.authservice.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+
+
 @Component
 public class JwtUtility {
-    private final String SECRET = "secret";
+
+
+    private final String SECRET = "my-super-secure-secret-key-12345678901234567890:";
     public String generateToken(String username) {
         Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
