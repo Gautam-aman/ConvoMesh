345636bbf277849f1d3ce74d83d8beafa0fa452a
Aman Gautam <amangautam2128@gmail.com>
2026-03-28 23:00:01 +0530

    Added cors config and simple html frontend

--- websocket-service/src/main/java/com/cfs/websocketservice/config/WebSocketConfig.java
+++ websocket-service/src/main/java/com/cfs/websocketservice/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.cfs.websocketservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.resilience.annotation.ConcurrencyLimit;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -14,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
         // with sockjs fallback mechnism on failing use http polling
         // origins react angu
     }
