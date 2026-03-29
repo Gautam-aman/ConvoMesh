61ed126ddb77a1b8d3649d799719dad4e2578dd7
Aman Gautam <amangautam2128@gmail.com>
2026-03-29 18:15:48 +0530

    Configured kafka consumer

--- websocket-service/src/main/java/com/cfs/websocketservice/service/KafkaConsumer.java
+++ websocket-service/src/main/java/com/cfs/websocketservice/service/KafkaConsumer.java
@@ -2,12 +2,17 @@ package com.cfs.websocketservice.service;
 
 
 import com.cfs.websocketservice.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class KafkaConsumer {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final String CHAT_SERVICE_URL = "http://localhost:8082/chat/save";
@@ -16,6 +21,9 @@ public class KafkaConsumer {
     public void consume(ChatMessage message) {
         // save to db
         restTemplate.postForObject(CHAT_SERVICE_URL, message, ChatMessage.class);
+
+        messagingTemplate.convertAndSend("/topic/messages",
+                message);
 
         System.out.println("Saved message: " + message.getContent());
 
