b0d8af7319c61e832a4148e7fca1283b3c57b715
Aman Gautam <amangautam2128@gmail.com>
2026-04-03 22:55:02 +0530

    implementing jwt auth

---
+++ Gateway/src/main/java/config/JwtUtil.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtUtil {
+    private final String secret = "secret";
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token);
+            return true;
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+}
